038f22189b2eca0b83c3016ce33a96715c6e67be
Umair <ubaig517@gmail.com>
2018-08-23 13:24:20 -0400

    Created routes for posts and new posts. Assimilating Link to navigate. Installed redux-form for form architexture

---
+++ src/components/posts_new.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+//import Link from 'react-router-dom';
+
+class PostsNew extends Component {
+  render() {
+    return (
+      <div>
+        PostsNew!
+      </div>
+    );
+  }
+}
+
+export default PostsNew;
